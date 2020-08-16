b91768c02b44a174167b0a3a503cf37df39b3a0c
Anthony Okpako <melowsworld@yahoo.co.uk>
2020-08-16 22:03:22 +0100

    Initial Commit

---
+++ Netflix-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Netflix-Clone
+//
+//  Created by User on 16/08/2020.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
